1111a210f94d2c87c8a0a6a5501aa24f95c28119
juancarloss98 <juancarloss@unicauca.edu.co>
2019-10-15 21:48:53 -0500

    Primera Subida

---
+++ ServidorCentral/src/servidorcentral/main/ServidorCentral.java
@@ -0,0 +1,17 @@
+
+package servidorcentral.main;
+
+/**
+ *
+ * @author JUAN
+ */
+public class ServidorCentral {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
